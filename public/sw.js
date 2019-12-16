cfcdd4d4cf0c4a0bcfa313455f78fd7223f87c9e
MIYA <19277961+kmiyabbm@users.noreply.github.com>
2019-12-16 19:11:35 +0900

    [add] プレキャッシュを導入した

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,22 @@
 self.addEventListener('fetch', function(e) {
     // ここは空でもOK
   })
+
+// ファイブラリのインポート
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '12346'
+  },
+  {
+    url: '/app.css',
+    revision: '12346'
+  },
+  { 
+　url: '/app.js',
+    revision: '12346'
+  },
+])
